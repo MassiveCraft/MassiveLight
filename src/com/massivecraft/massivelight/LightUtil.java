25759eaa0df509ed99fca1394d145389a4620023
Olof Larsson <olof@sylt.nu>
2015-03-24 21:59:24 +0100

    New Minecraft NMS

--- src/com/massivecraft/massivelight/LightUtil.java
+++ src/com/massivecraft/massivelight/LightUtil.java
@@ -1,11 +1,11 @@
 package com.massivecraft.massivelight;
 
-import net.minecraft.server.v1_8_R1.BlockPosition;
-import net.minecraft.server.v1_8_R1.WorldServer;
+import net.minecraft.server.v1_8_R2.BlockPosition;
+import net.minecraft.server.v1_8_R2.WorldServer;
 
 import org.bukkit.World;
 import org.bukkit.block.Block;
-import org.bukkit.craftbukkit.v1_8_R1.CraftWorld;
+import org.bukkit.craftbukkit.v1_8_R2.CraftWorld;
 
 /**
  * Find the method by going through CraftBlock.setTypeId()
