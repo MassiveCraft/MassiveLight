becdb1627790c20f79f7c0c32f2de7cc1b7b3383
Olof Larsson <olof@sylt.nu>
2016-06-23 11:55:40 +0200

    MassiveCore - Automatic Collection Constructors

--- src/com/massivecraft/massivelight/entity/MConfColl.java
+++ src/com/massivecraft/massivelight/entity/MConfColl.java
@@ -2,9 +2,6 @@ package com.massivecraft.massivelight.entity;
 
 import com.massivecraft.massivecore.MassiveCore;
 import com.massivecraft.massivecore.store.Coll;
-import com.massivecraft.massivecore.store.MStore;
-import com.massivecraft.massivelight.Const;
-import com.massivecraft.massivelight.MassiveLight;
 
 public class MConfColl extends Coll<MConf>
 {
@@ -14,10 +11,6 @@ public class MConfColl extends Coll<MConf>
 	
 	private static MConfColl i = new MConfColl();
 	public static MConfColl get() { return i; }
-	private MConfColl()
-	{
-		super(Const.COLLECTION_MCONF, MConf.class, MStore.getDb(), MassiveLight.get());
-	}
 
 	// -------------------------------------------- //
 	// STACK TRACEABILITY
